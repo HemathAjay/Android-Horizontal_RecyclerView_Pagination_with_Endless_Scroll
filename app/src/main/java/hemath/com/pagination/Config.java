af638175768ad9e11f78dce1b09bc3a4fab21529
Hemathajay <26480808+HemathAjay@users.noreply.github.com>
2019-04-30 13:35:40 +0530

    Update Config.java

--- app/src/main/java/hemath/com/pagination/Config.java
+++ app/src/main/java/hemath/com/pagination/Config.java
@@ -2,7 +2,7 @@ package hemath.com.pagination;
 
 public class Config
 {
-    public static final String DATA_URL = "http://nojservice2.noveljunction.com/api/GetBookByPaging?iPageIndex=";
+    public static final String DATA_URL = "http://es.example.com/api/GetDataByPaging?iPageIndex=";
     public static final String TAG_IMAGE_URL = "FullImageFile";
     public static final String TAG_TITLE = "Title";
 }
